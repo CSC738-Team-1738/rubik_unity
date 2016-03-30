f9da47a4a140f560ffbc9438b316af37e52d42e4
W738806 <d.lake.smith@eagles.usm.edu>
2016-03-30 15:57:15 -0500

    Initial commit

---
+++ Assets/Scripts/Cubie.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cubie : MonoBehaviour {
+
+	public Cubie(Color faceAColor, Color faceBColor, Color faceCColor) {
+		Material materialA, materialB, materialC;
+
+		materialA = GameObject.Find("FaceA").GetComponent<Renderer>().material;
+		materialB = GameObject.Find("FaceB").GetComponent<Renderer>().material;
+		materialC = GameObject.Find("FaceC").GetComponent<Renderer>().material;
+
+		materialA.color = faceAColor;
+		materialB.color = faceBColor;
+		materialA.color = faceCColor;
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
